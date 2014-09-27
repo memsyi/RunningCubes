56784b7cf1cb1ffe0730a69f685d79d835ceb9eb
RuiZhuo <339361504@qq.com>
2014-09-27 23:07:16 +0800

    fix by Rui Zhuo
    delete NGUI
    测试开始界面切换到游戏界面

---
+++ Assets/Script/CFirstUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CFirstUI : MonoBehaviour {
+	public Texture image;
+	public GUISkin testSkin;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI (){
+		GUI.skin = testSkin;
+		GUI.Label (new Rect (Screen.width / 2 - 50, Screen.height / 3, 100, 100), "Running Cubes");
+		if(GUI.Button (new Rect (Screen.width / 2, Screen.height / 2, 100, 100), image))
+			Application.LoadLevel("GameScene") ;
+	}
+
+}
